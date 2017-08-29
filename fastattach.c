916e853ccee139d2fffb5ae6d79bb505991b8bc9
John Lightsey <john@nixnuts.net>
2017-08-28 21:19:46 -0500

    Properly implement libfastattach.
    This make fastattach skip the next ptrace() attach attempt so that gdb
    can work with the already-attached process. It also switches to argv
    parsing to determine the target pid.

--- fastattach.c
+++ fastattach.c
@@ -1,31 +1,44 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <limits.h>
-#include <sys/ptrace.h>
+#include <string.h>
 
+#define __USE_GNU
+#include <dlfcn.h>
 
-void _attach_to_pid(pid_t target_pid) {
-    if (ptrace(PTRACE_ATTACH, target_pid, 0, 0) == -1) {
-        perror("Failed to attach to pid");
+#define PTRACE_ATTACH  16
+
+static int skip_next_attach = 0;
+
+long ptrace(int request, pid_t pid, void *addr, void *data)
+{
+    static long (*ptrace_real) (int request, pid_t pid, void *addr, void *data) = NULL;
+    if (ptrace_real == NULL)
+        ptrace_real = dlsym(RTLD_NEXT, "ptrace");
+
+    if (skip_next_attach && request == PTRACE_ATTACH) {
+        skip_next_attach = 0;
+        return 0;
     }
-    else {
-        printf("Fast-attached to pid %lu\n", target_pid);
-    }
+    return ptrace_real(request, pid, addr, data);
 }
 
-void fastattach() {
-    char *target_pid_env = NULL;
+void fastattach(int argc, char **argv, char **env)
+{
+    char **target_arg;
     pid_t target_pid = 0;
 
-    target_pid_env = getenv("TARGET_PID");
-    if (target_pid_env != NULL) {
-        target_pid = strtoul(target_pid_env, NULL, 10);
-        if (target_pid == 0 || target_pid == ULONG_MAX) {
-            printf("Invalid value for target pid: %s\n", target_pid_env);
+    for (target_arg = argv; *target_arg != NULL; target_arg++) {
+        if (strcmp(*target_arg, "-p") == 0) {
+            target_arg++;
+            break;
         }
-        else {
-            _attach_to_pid(target_pid);
+    }
+    if (*target_arg != NULL) {
+        target_pid = strtoul(*target_arg, NULL, 10);
+        if (target_pid != 0 && target_pid != ULONG_MAX && ptrace(PTRACE_ATTACH, target_pid, 0, 0) == 0) {
+            printf("Fast-attached to pid %lu\n", target_pid);
+            skip_next_attach = 1;
         }
     }
 }
-
